be249f5df2ec4f5acaa7abaa1bd68e332fca833a
Babatunde <Babatunde@nlng.com>
2026-05-09 20:55:31 +0100

    updating cloudwatch permission

--- terraform/modules/ec2_app/main.tf
+++ terraform/modules/ec2_app/main.tf
@@ -126,29 +126,9 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-resource "aws_iam_policy" "cloudwatch_logs" {
-  name = "${var.project_name}-cloudwatch-logs-policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "logs:DescribeLogStreams"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
-
 resource "aws_iam_role_policy_attachment" "cloudwatch_logs" {
   role       = aws_iam_role.ec2_role.name
-  policy_arn = aws_iam_policy.cloudwatch_logs.arn
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
